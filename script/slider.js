6ef226086df488a0a51748e8c33504d058c49f0e
Vladimir Yablokov <YablokovWork@gmail.com>
2022-03-30 00:06:34 +0300

    change css and slider.js(w:100% h:100vh)

--- script/slider.js
+++ script/slider.js
@@ -1,6 +1,6 @@
-const rootElem = document.querySelector('#root2');
+const rootElem = document.querySelector('#root1');
 const imgList = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpeg'];
-const mediaPath = 'media/'
+const mediaPath = 'media/';
 let imgIndex = 0;
 
 const slider_container = document.createElement('div');
@@ -19,42 +19,49 @@ slider_trigger.classList.add('slider-trigger');
 slider_film.classList.add('slider-film');
 
 slider_trigger.append(slider_trigger_left, slider_trigger_right);
-
 slider_container.append(slider_main, slider_trigger);
 slider_main.append(slider_film);
 rootElem.append(slider_container);
 
-slider_film.append(...imgList.map(img_name=>{
+const film_elems = imgList.map(img_name => {
+	const slider_width = slider_container.offsetWidth;
 	const divElem = document.createElement('div');
+	divElem.style.width = slider_width + 'px';
 	divElem.style.backgroundImage = `url('${mediaPath+img_name}')`;
-	return divElem
-}));
+	return divElem;
+});
 
+const render = () => {
+	const slider_width = slider_container.offsetWidth;
+	slider_film.style.right = slider_width * imgIndex + 'px';
+};
 
+const changeSize = () => {
+	const slider_width = slider_container.offsetWidth;
+	slider_film.style.width = slider_width * imgList.length + 'px';
+	film_elems.forEach(elem => elem.style.width = slider_width + 'px');
+	render();
+};
 
-const render = ()=>{
-	const window_width = slider_main.offsetWidth;
-	slider_film.style.right = window_width * imgIndex +'px'
-}
+window.addEventListener('resize', changeSize);
+changeSize();
 
+slider_film.append(...film_elems);
 
-
-slider_trigger_left.addEventListener('click', ()=>{
-	if (imgIndex > 0){
+slider_trigger_left.addEventListener('click', () => {
+	if (imgIndex > 0) {
 		imgIndex--;
-	}else{
+	} else {
 		imgIndex = imgList.length - 1;
 	}
-	render()
+	render();
 });
 
-slider_trigger_right.addEventListener('click', ()=>{
-	if (imgList.length - 1 > imgIndex){
+slider_trigger_right.addEventListener('click', () => {
+	if (imgList.length - 1 > imgIndex) {
 		imgIndex++;
-	}else{
+	} else {
 		imgIndex = 0;
 	}
-	render()
+	render();
 });
-
-
